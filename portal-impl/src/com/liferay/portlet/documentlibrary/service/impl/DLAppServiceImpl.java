865e8fdf804986d4dde428da6806c85080fa9991
David Truong <david.truong@liferay.com>
2023-07-19 00:41:45 -0500

    LPS-185551 create temp folders in production

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
@@ -31,6 +31,7 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.bean.BeanReference;
 import com.liferay.portal.kernel.cache.thread.local.ThreadLocalCachable;
+import com.liferay.portal.kernel.change.tracking.CTAware;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.NoSuchGroupException;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -2297,6 +2298,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 * @see    #addTempFileEntry(long, long, String, String, File, String)
 	 * @see    TempFileEntryUtil
 	 */
+	@CTAware(onProduction = true)
 	@Override
 	public String[] getTempFileNames(
 			long groupId, long folderId, String folderName)
--- portal-kernel/src/com/liferay/document/library/kernel/service/DLAppService.java
+++ portal-kernel/src/com/liferay/document/library/kernel/service/DLAppService.java
@@ -1570,6 +1570,7 @@ public interface DLAppService extends BaseService {
 	 * @see #addTempFileEntry(long, long, String, String, File, String)
 	 * @see TempFileEntryUtil
 	 */
+	@CTAware(onProduction = true)
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public String[] getTempFileNames(
 			long groupId, long folderId, String folderName)
