5a2ea9689d6bc8b3e8cc5d548cfa200638ce3da6
Cheryl Tang <cheryl.tang@liferay.com>
2023-09-07 15:13:15 -0700

    LPS-187183 Add missing key prop to some pre-existing list elements

--- modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/views/ChangeTrackingChangesView.js
+++ modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/views/ChangeTrackingChangesView.js
@@ -1693,6 +1693,7 @@ export default function ChangeTrackingChangesView({
 			rows.push(
 				<ClayTable.Row
 					className="cursor-pointer"
+					key={node.nodeId}
 					onClick={() => navigate(node.nodeId)}
 				>
 					<ClayTable.Cell>
