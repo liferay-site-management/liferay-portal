762ea97bb8f80c38d77c7a65a5e268953ad480aa
brookedalton <brooke.dalton@liferay.com>
2023-10-05 16:54:04 -0700

    LPS-133973 Add SocialActivityTableReferenceDefinitionTest

---
+++ modules/apps/social/social-activity-test/src/testIntegration/java/com/liferay/social/activity/change/tracking/test/SocialActivityTableReferenceDefinitionTest.java
@@ -0,0 +1,67 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.social.activity.change.tracking.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.change.tracking.test.util.BaseTableReferenceDefinitionTestCase;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.social.activity.service.test.util.SocialActivityTestUtil;
+import com.liferay.social.kernel.model.SocialActivity;
+import com.liferay.social.kernel.service.SocialActivityLocalServiceUtil;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Brooke Dalton
+ */
+@RunWith(Arquillian.class)
+public class SocialActivityTableReferenceDefinitionTest
+	extends BaseTableReferenceDefinitionTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_user = UserTestUtil.addUser(group.getGroupId());
+
+		_assetEntry = SocialActivityTestUtil.addAssetEntry(_user, group, null);
+	}
+
+	@Override
+	protected CTModel<?> addCTModel() throws Exception {
+		SocialActivityTestUtil.addActivity(_user, group, _assetEntry, 1);
+
+		List<SocialActivity> activities =
+			SocialActivityLocalServiceUtil.getGroupActivities(
+				group.getGroupId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		return activities.get(0);
+	}
+
+	private static AssetEntry _assetEntry;
+	private static User _user;
+
+}
