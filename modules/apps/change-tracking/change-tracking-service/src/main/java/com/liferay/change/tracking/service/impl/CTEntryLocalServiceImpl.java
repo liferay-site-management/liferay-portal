e20d281cc94ffe7abcf3128e42a186c369817886
pei-jung <pei-jung.lan@liferay.com>
2023-10-23 10:09:08 -0700

    LPS-198982 Add api to get ctEntries by workflow statuses

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTEntryLocalServiceImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTEntryLocalServiceImpl.java
@@ -12,6 +12,7 @@ import com.liferay.change.tracking.model.CTEntry;
 import com.liferay.change.tracking.model.CTEntryTable;
 import com.liferay.change.tracking.service.base.CTEntryLocalServiceBaseImpl;
 import com.liferay.change.tracking.service.persistence.CTCollectionPersistence;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.petra.sql.dsl.query.DSLQuery;
 import com.liferay.portal.aop.AopService;
@@ -20,14 +21,29 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.search.document.Document;
+import com.liferay.portal.search.hits.SearchHits;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.portal.search.sort.FieldSort;
+import com.liferay.portal.search.sort.SortFieldBuilder;
+import com.liferay.portal.search.sort.SortOrder;
+import com.liferay.portal.search.sort.Sorts;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
@@ -119,6 +135,97 @@ public class CTEntryLocalServiceImpl extends CTEntryLocalServiceBaseImpl {
 
 		return ctEntryPersistence.findByCtCollectionId(
 			ctCollectionId, start, end, orderByComparator);
+	}
+
+	@Override
+	public List<CTEntry> getCTCollectionCTEntries(
+			long ctCollectionId, LinkedHashMap<String, Object> params,
+			int[] statuses, int start, int end,
+			OrderByComparator<CTEntry> orderByComparator)
+		throws PortalException {
+
+		CTCollection ctCollection = _ctCollectionPersistence.findByPrimaryKey(
+			ctCollectionId);
+
+		SearchRequestBuilder searchRequestBuilder =
+			_searchRequestBuilderFactory.builder();
+
+		searchRequestBuilder.entryClassNames(
+			CTEntry.class.getName()
+		).emptySearchEnabled(
+			true
+		).highlightEnabled(
+			false
+		).withSearchContext(
+			searchContext -> {
+				searchContext.setCompanyId(ctCollection.getCompanyId());
+
+				searchContext.setAttribute("ctCollectionId", ctCollectionId);
+
+				if (ArrayUtil.isNotEmpty(statuses)) {
+					searchContext.setAttribute("statuses", statuses);
+				}
+
+				if (MapUtil.isEmpty(params)) {
+					return;
+				}
+
+				searchContext.setAttribute(
+					"showHideable", (boolean)params.get("showHideable"));
+			}
+		);
+
+		if (start != QueryUtil.ALL_POS) {
+			searchRequestBuilder.from(start);
+			searchRequestBuilder.size(end - start);
+		}
+
+		if (orderByComparator != null) {
+			SortOrder sortOrder = SortOrder.ASC;
+
+			if (!orderByComparator.isAscending()) {
+				sortOrder = SortOrder.DESC;
+			}
+
+			String[] orderByFields = orderByComparator.getOrderByFields();
+
+			FieldSort[] fieldSorts = new FieldSort[orderByFields.length];
+
+			for (int i = 0; i < orderByFields.length; i++) {
+				fieldSorts[i] = _sorts.field(
+					_sortFieldBuilder.getSortField(
+						CTEntry.class, orderByFields[i]),
+					sortOrder);
+			}
+
+			searchRequestBuilder.sorts(fieldSorts);
+		}
+
+		SearchResponse searchResponse = _searcher.search(
+			searchRequestBuilder.build());
+
+		SearchHits searchHits = searchResponse.getSearchHits();
+
+		return TransformUtil.transform(
+			searchHits.getSearchHits(),
+			searchHit -> {
+				Document document = searchHit.getDocument();
+
+				long ctEntryId = document.getLong(Field.ENTRY_CLASS_PK);
+
+				CTEntry ctEntry = fetchCTEntry(ctEntryId);
+
+				if (ctEntry == null) {
+					Indexer<CTEntry> indexer = IndexerRegistryUtil.getIndexer(
+						CTEntry.class);
+
+					indexer.delete(
+						document.getLong(Field.COMPANY_ID),
+						document.getString(Field.UID));
+				}
+
+				return ctEntry;
+			});
 	}
 
 	@Override
@@ -271,5 +378,17 @@ public class CTEntryLocalServiceImpl extends CTEntryLocalServiceBaseImpl {
 
 	@Reference
 	private CTCollectionPersistence _ctCollectionPersistence;
+
+	@Reference
+	private Searcher _searcher;
+
+	@Reference
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+	@Reference
+	private SortFieldBuilder _sortFieldBuilder;
+
+	@Reference
+	private Sorts _sorts;
 
 }
