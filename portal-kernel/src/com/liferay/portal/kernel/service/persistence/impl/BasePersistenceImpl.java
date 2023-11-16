838af1b2c7ebda934383d7952e49be15f6234cdf
David Truong <david.truong@liferay.com>
2023-11-15 18:13:29 -0600

    LPS-201585 add cache for CT mode

--- portal-kernel/src/com/liferay/portal/kernel/service/persistence/impl/BasePersistenceImpl.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/persistence/impl/BasePersistenceImpl.java
@@ -6,6 +6,7 @@
 package com.liferay.portal.kernel.service.persistence.impl;
 
 import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.sql.dsl.Column;
 import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.petra.sql.dsl.Table;
@@ -30,6 +31,7 @@ import com.liferay.petra.sql.dsl.spi.query.SetOperation;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
+import com.liferay.portal.kernel.change.tracking.cache.CTCacheThreadLocal;
 import com.liferay.portal.kernel.configuration.Configuration;
 import com.liferay.portal.kernel.configuration.Filter;
 import com.liferay.portal.kernel.dao.db.DB;
@@ -219,11 +221,16 @@ public class BasePersistenceImpl<T extends BaseModel<T>>
 
 		Object[] arguments = _getArguments(defaultASTNodeListener);
 
-		Object cacheResult = finderCache.getResult(finderPath, arguments, this);
+		Object cacheResult = null;
 
-		boolean productionMode = CTCollectionThreadLocal.isProductionMode();
+		try (SafeCloseable safeCloseable =
+				CTCacheThreadLocal.setCTCacheEnabledWithSafeCloseable(
+					!CTCollectionThreadLocal.isProductionMode())) {
 
-		if ((cacheResult != null) && productionMode) {
+			cacheResult = finderCache.getResult(finderPath, arguments, this);
+		}
+
+		if (cacheResult != null) {
 			return (R)cacheResult;
 		}
 
@@ -298,9 +305,7 @@ public class BasePersistenceImpl<T extends BaseModel<T>>
 					defaultASTNodeListener.getEnd());
 			}
 
-			if (productionMode) {
-				finderCache.putResult(finderPath, arguments, result);
-			}
+			finderCache.putResult(finderPath, arguments, result);
 
 			return (R)result;
 		}
