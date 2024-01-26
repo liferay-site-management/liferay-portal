4d7871a2d8cb7739c28cdf7452975c43c1b30170
pei-jung <pei-jung.lan@liferay.com>
2024-01-26 14:45:47 -0800

    LPD-17055 If deletion protection is enabled, object cannot be deleted if it's being modified in other publications

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/CTPersistenceHelperImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/CTPersistenceHelperImpl.java
@@ -10,12 +10,14 @@ import com.liferay.change.tracking.model.CTEntry;
 import com.liferay.change.tracking.service.CTEntryLocalService;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
+import com.liferay.portal.kernel.change.tracking.CTRequiredModelException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.change.tracking.CTModel;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.persistence.change.tracking.helper.CTPersistenceHelper;
+import com.liferay.portal.util.PropsValues;
 
 import java.io.Serializable;
 
@@ -123,14 +125,26 @@ public class CTPersistenceHelperImpl implements CTPersistenceHelper {
 
 		long ctCollectionId = CTCollectionThreadLocal.getCTCollectionId();
 
-		if (ctCollectionId == CTConstants.CT_COLLECTION_ID_PRODUCTION) {
-			return true;
-		}
-
 		long modelClassNameId = _classNameLocalService.getClassNameId(
 			ctModel.getModelClass());
 
 		long modelClassPK = ctModel.getPrimaryKey();
+
+		if (PropsValues.CHANGE_TRACKING_DELETION_PROTECTION_ENABLED &&
+			_ctEntryLocalService.hasCTEntries(
+				ctCollectionId, modelClassNameId, modelClassPK,
+				CTConstants.CT_CHANGE_TYPE_MODIFICATION)) {
+
+			throw new CTRequiredModelException(
+				String.format(
+					"Model %s %s cannot be deleted because it is currently " +
+						"being modified in other publication(s)",
+					ctModel.getModelClassName(), modelClassPK));
+		}
+
+		if (ctCollectionId == CTConstants.CT_COLLECTION_ID_PRODUCTION) {
+			return true;
+		}
 
 		CTEntry ctEntry = _ctEntryLocalService.fetchCTEntry(
 			ctCollectionId, modelClassNameId, modelClassPK);
