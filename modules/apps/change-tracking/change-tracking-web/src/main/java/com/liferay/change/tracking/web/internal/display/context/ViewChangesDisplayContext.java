7e51e7304e2ce25c7a3c6a76df4cd5116a73cee2
Noor Najjar <noor.najjar@liferay.com>
2024-03-22 12:02:43 -0700

    LPD-19748 Add workflow info to review change view

--- modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/display/context/ViewChangesDisplayContext.java
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/display/context/ViewChangesDisplayContext.java
@@ -1455,6 +1455,16 @@ public class ViewChangesDisplayContext {
 						"groupId", groupedModel.getGroupId());
 				}
 
+				int changeType = _ctDisplayRendererRegistry.getChangeType(
+					ctEntry, model);
+
+				if (_ctDisplayRendererRegistry.isWorkflowEnabled(
+						ctEntry, model) &&
+					(changeType != CTConstants.CT_CHANGE_TYPE_DELETION)) {
+
+					modelInfo._jsonObject.put("showWorkflow", true);
+				}
+
 				modelInfo._site = _isSite(model);
 			}
 		}
