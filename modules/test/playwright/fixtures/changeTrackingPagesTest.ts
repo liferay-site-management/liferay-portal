d76c3503466a6eb3f45b5de98fecdac11e27d7e7
brookedalton <brooke.dalton@liferay.com>
2024-04-05 13:39:26 -0700

    LPD-20002 Move publication setup and teardown to fixture

--- modules/test/playwright/fixtures/changeTrackingPagesTest.ts
+++ modules/test/playwright/fixtures/changeTrackingPagesTest.ts
@@ -3,16 +3,61 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {test} from '@playwright/test';
+import {mergeTests, test} from '@playwright/test';
 
+import {ApiHelpers} from '../helpers/ApiHelpers';
 import {ChangeTrackingPage} from '../pages/change-tracking-web/ChangeTrackingPage';
+import getRandomString from '../utils/getRandomString';
+import {loginTest} from './loginTest';
 
-const changeTrackingPagesTest = test.extend<{
+const changeTrackingPages = test.extend<{
 	changeTrackingPage: ChangeTrackingPage;
+	ctCollection;
 }>({
 	changeTrackingPage: async ({page}, use) => {
 		await use(new ChangeTrackingPage(page));
 	},
+	ctCollection: [
+		async ({page}, use) => {
+			await page.goto('/');
+
+			const apiHelpers = new ApiHelpers(page);
+
+			let ctCollection;
+
+			try {
+
+				// Create ctCollection
+
+				ctCollection =
+					await apiHelpers.headlessChangeTracking.createCTCollection(
+						getRandomString()
+					);
+
+				// Checkout ctCollection
+
+				await apiHelpers.headlessChangeTracking.checkoutCTCollection(
+					ctCollection.id
+				);
+
+				await use(ctCollection);
+			}
+			catch {
+				throw new Error(`Could not checkout ctCollection`);
+			}
+			finally {
+
+				// Delete ctCollection
+
+				await apiHelpers.headlessChangeTracking.deleteCTCollection(
+					ctCollection.id
+				);
+			}
+		},
+		{auto: true},
+	],
 });
+
+const changeTrackingPagesTest = mergeTests(loginTest(), changeTrackingPages);
 
 export {changeTrackingPagesTest};
--- modules/test/playwright/tests/change-tracking-web/viewChangesWorkflow.spec.ts
+++ modules/test/playwright/tests/change-tracking-web/viewChangesWorkflow.spec.ts
@@ -5,80 +5,41 @@
 
 import {expect, mergeTests} from '@playwright/test';
 
-import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {changeTrackingPagesTest} from '../../fixtures/changeTrackingPagesTest';
 import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
-import {loginTest} from '../../fixtures/loginTest';
 import {workflowPagesTest} from '../../fixtures/workflowPagesTest';
 import getRandomString from '../../utils/getRandomString';
 import {journalPagesTest} from '../journal-web/fixtures/journalPagesTest';
 
 export const test = mergeTests(
-	apiHelpersTest,
 	featureFlagsTest({
 		'LPD-10703': true,
 	}),
 	journalPagesTest,
 	changeTrackingPagesTest,
-	workflowPagesTest,
-	loginTest()
+	workflowPagesTest
 );
 
-let ctCollection;
 let journalName;
-let publicationName;
 
-test.beforeEach(
-	async ({
-		apiHelpers,
-		changeTrackingPage,
-		journalEditArticlePage,
-		workflowPage,
-	}) => {
-		await workflowPage.goto();
-
-		await workflowPage.changeWorkflow(
-			'Web Content Article',
-			'Single Approver'
-		);
-
-		await changeTrackingPage.enablePublications();
-
-		publicationName = getRandomString();
-
-		ctCollection =
-			await apiHelpers.headlessChangeTracking.createCTCollection(
-				publicationName
-			);
-
-		await apiHelpers.headlessChangeTracking.checkoutCTCollection(
-			ctCollection.id
-		);
-
-		journalName = getRandomString();
-
-		await journalEditArticlePage.goto();
-		await journalEditArticlePage.submitArticleForWorkflow(journalName);
-	}
-);
-
-test.afterEach(async ({apiHelpers, changeTrackingPage, workflowPage}) => {
-	await apiHelpers.headlessChangeTracking.deleteCTCollection(ctCollection.id);
-
-	await changeTrackingPage.disablePublications();
-
+test.beforeEach(async ({journalEditArticlePage, workflowPage}) => {
 	await workflowPage.goto();
 
-	await workflowPage.changeWorkflow('Web Content Article', 'No Workflow', {
-		disable: true,
-	});
+	await workflowPage.changeWorkflow('Web Content Article', 'Single Approver');
+
+	journalName = getRandomString();
+
+	await journalEditArticlePage.goto();
+
+	await journalEditArticlePage.submitArticleForWorkflow(journalName);
 });
 
 test('LPD-19748 Add workflow info to the View Change screen', async ({
 	changeTrackingPage,
+	ctCollection,
 	page,
 }) => {
-	await changeTrackingPage.goToReviewChanges(publicationName);
+	await changeTrackingPage.goToReviewChanges(ctCollection.name);
 
 	await changeTrackingPage.reviewChange(journalName);
 
@@ -89,6 +50,7 @@ test('LPD-19748 Add workflow info to the View Change screen', async ({
 
 test('LPD-19748 Workflow data is displayed in tab', async ({
 	changeTrackingPage,
+	ctCollection,
 	page,
 }) => {
 	const displayData = [
@@ -99,7 +61,7 @@ test('LPD-19748 Workflow data is displayed in tab', async ({
 		'Due Date',
 	];
 
-	await changeTrackingPage.goToReviewChanges(publicationName);
+	await changeTrackingPage.goToReviewChanges(ctCollection.name);
 
 	await changeTrackingPage.reviewChange(journalName);
 
@@ -112,6 +74,7 @@ test('LPD-19748 Workflow data is displayed in tab', async ({
 
 test('LPD-19748 Only workflow status is displayed when workflow is disabled', async ({
 	changeTrackingPage,
+	ctCollection,
 	page,
 	workflowPage,
 }) => {
@@ -121,7 +84,7 @@ test('LPD-19748 Only workflow status is displayed when workflow is disabled', as
 		disable: true,
 	});
 
-	await changeTrackingPage.goToReviewChanges(publicationName);
+	await changeTrackingPage.goToReviewChanges(ctCollection.name);
 
 	await changeTrackingPage.reviewChange(journalName);
 
