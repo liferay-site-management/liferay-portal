bd640ce07a9ee0223ab60b150e883f2e8e92fb03
pei-jung <pei-jung.lan@liferay.com>
2024-04-25 11:38:07 -0700

    LPD-21520 Refresh component after assignment and preserve the selected tab

--- modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/views/ChangeTrackingRenderView.js
+++ modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/views/ChangeTrackingRenderView.js
@@ -161,11 +161,16 @@ export default function ChangeTrackingRenderView({
 
 	const [dataURL, setDataURL] = useState(initialDataURL);
 	const [loading, setLoading] = useState(false);
+	const [refresh, setRefresh] = useState({});
 	const [selectedLocale, setSelectedLocale] = useState(defaultLocale);
 	const [
 		selectedSegmentsExperienceId,
 		setSelectedSegmentsExperienceId,
 	] = useState(null);
+	const [
+		showWorkflowSuccessMessage,
+		setShowWorkflowSuccessMessage,
+	] = useState(false);
 	const [state, setState] = useState({
 		contentType: CONTENT_TYPE_PREVIEW,
 		renderData: null,
@@ -193,7 +198,6 @@ export default function ChangeTrackingRenderView({
 
 				const newState = {
 					children: childEntries,
-					contentType: CONTENT_TYPE_PREVIEW,
 					parents: parentEntries,
 					renderData: json,
 					view: VIEW_UNIFIED,
@@ -253,7 +257,7 @@ export default function ChangeTrackingRenderView({
 					newState.view = VIEW_SPLIT;
 				}
 
-				setState(newState);
+				setState((prevState) => ({...prevState, ...newState}));
 
 				setLoading(false);
 			})
@@ -267,7 +271,28 @@ export default function ChangeTrackingRenderView({
 					},
 				});
 			});
-	}, [childEntries, dataURL, parentEntries, selectedSegmentsExperienceId]);
+	}, [
+		childEntries,
+		dataURL,
+		parentEntries,
+		refresh,
+		selectedSegmentsExperienceId,
+	]);
+
+	useEffect(() => {
+		if (showWorkflowSuccessMessage) {
+			Liferay.fire('closeModal');
+
+			setRefresh({});
+
+			openToast({
+				message: Liferay.Language.get(
+					'your-request-completed-successfully'
+				),
+				type: 'success',
+			});
+		}
+	}, [showWorkflowSuccessMessage]);
 
 	let currentLocale = selectedLocale;
 	let currentTitle = title;
@@ -807,18 +832,12 @@ export default function ChangeTrackingRenderView({
 
 									iframe.contentWindow.location.reload();
 
-									Liferay.fire('closeModal');
-
-									openToast({
-										message: Liferay.Language.get(
-											'your-request-completed-successfully'
-										),
-										type: 'success',
-									});
+									setShowWorkflowSuccessMessage(true);
 								},
 							},
 						],
 						height: workflowAction.modalHeight,
+						onOpen: () => setShowWorkflowSuccessMessage(false),
 						size: 'lg',
 						title: workflowAction.label,
 						url: workflowAction.href,
