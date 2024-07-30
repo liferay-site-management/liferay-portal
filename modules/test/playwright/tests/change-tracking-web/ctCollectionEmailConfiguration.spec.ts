8975384cd714989d887f77c274f5479ec44c4399
brookedalton <brooke.dalton@liferay.com>
2024-07-30 16:14:32 -0700

    LPD-28956 Add playwright test for configuration page

--- modules/test/playwright/tests/change-tracking-web/ctCollectionEmailConfiguration.spec.ts
+++ modules/test/playwright/tests/change-tracking-web/ctCollectionEmailConfiguration.spec.ts
@@ -5,10 +5,12 @@
 
 import {expect, mergeTests} from '@playwright/test';
 
+import {changeTrackingPagesTest} from '../../fixtures/changeTrackingPagesTest';
 import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {loginTest} from '../../fixtures/loginTest';
 
 export const test = mergeTests(
+	changeTrackingPagesTest,
 	featureFlagsTest({
 		'LPD-11212': true,
 	}),
@@ -45,4 +47,39 @@ test('LPD-28802 Verify email notification checkbox is displayed', async ({
 
 	await expect(invitePublication.getByRole('checkbox').nth(0)).toBeChecked();
 	await expect(invitePublication.getByRole('checkbox').nth(1)).toBeChecked();
+});
+
+test('LPD-28956 Verify the configuration fields are displayed', async ({
+	ChangeTrackingInstanceSettingsPage,
+	page,
+}) => {
+	await ChangeTrackingInstanceSettingsPage.goto(
+		'Publications Email Notifications'
+	);
+
+	await expect(
+		page.getByRole('heading', {name: 'Publications Email Notifications'})
+	).toBeVisible();
+
+	const displayData = [
+		'Email from Address',
+		'Email from Name',
+		'Invitation Email Subject',
+		'Invitation Email Body',
+	];
+
+	for (const data of displayData) {
+		await expect(page.getByText(data, {exact: true})).toBeVisible();
+	}
+
+	const subject =
+		'[$PORTAL_URL$]: You Have Been Invited to Work on a Publication';
+	const body =
+		'Dear [$TO_NAME$],<br /><br /> You have been invited to work on a publication. ' +
+		'For further information, please visit [$PORTAL_PUBLICATION_REVIEW_CHANGES_URL$].' +
+		'<br /><br /> Sincerely,<br /> [$FROM_NAME$]<br /> [$FROM_ADDRESS$]<br /> [$PORTAL_URL$]<br />';
+
+	await expect(page.getByText(subject)).toBeVisible();
+
+	await expect(page.getByText(body)).toBeVisible();
 });
