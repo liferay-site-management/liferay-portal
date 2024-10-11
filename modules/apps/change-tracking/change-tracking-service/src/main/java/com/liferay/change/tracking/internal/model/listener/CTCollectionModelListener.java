be685d329d43b4e2b992fe6ff8696b150f453eea
brookedalton <brooke.dalton@liferay.com>
2024-10-11 16:49:19 -0700

    LPD-38392 Reindex ctEntries if the name or status does not match

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/model/listener/CTCollectionModelListener.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/model/listener/CTCollectionModelListener.java
@@ -6,9 +6,12 @@
 package com.liferay.change.tracking.internal.model.listener;
 
 import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.model.CTEntry;
 import com.liferay.change.tracking.model.CTProcess;
+import com.liferay.change.tracking.service.CTEntryLocalService;
 import com.liferay.change.tracking.service.CTPreferencesLocalService;
 import com.liferay.change.tracking.service.CTProcessLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
@@ -17,10 +20,14 @@ import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.Ticket;
 import com.liferay.portal.kernel.model.TicketConstants;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.service.TicketLocalService;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -58,6 +65,29 @@ public class CTCollectionModelListener extends BaseModelListener<CTCollection> {
 			CTCollection originalCTCollection, CTCollection ctCollection)
 		throws ModelListenerException {
 
+		if (!Objects.equals(
+				originalCTCollection.getName(), ctCollection.getName()) ||
+			(originalCTCollection.getStatus() != ctCollection.getStatus())) {
+
+			Indexer<CTEntry> indexer = _indexerRegistry.getIndexer(
+				CTEntry.class);
+
+			if (indexer != null) {
+				for (CTEntry ctEntry :
+						_ctEntryLocalService.getCTCollectionCTEntries(
+							ctCollection.getCtCollectionId(), QueryUtil.ALL_POS,
+							QueryUtil.ALL_POS, null)) {
+
+					try {
+						indexer.reindex(ctEntry);
+					}
+					catch (SearchException searchException) {
+						throw new ModelListenerException(searchException);
+					}
+				}
+			}
+		}
+
 		if ((originalCTCollection.getStatus() != ctCollection.getStatus()) &&
 			(ctCollection.getStatus() == WorkflowConstants.STATUS_EXPIRED)) {
 
@@ -90,10 +120,16 @@ public class CTCollectionModelListener extends BaseModelListener<CTCollection> {
 		CTCollectionModelListener.class.getName());
 
 	@Reference
+	private CTEntryLocalService _ctEntryLocalService;
+
+	@Reference
 	private CTPreferencesLocalService _ctPreferencesLocalService;
 
 	@Reference
 	private CTProcessLocalService _ctProcessLocalService;
+
+	@Reference
+	private IndexerRegistry _indexerRegistry;
 
 	@Reference
 	private TicketLocalService _ticketLocalService;
