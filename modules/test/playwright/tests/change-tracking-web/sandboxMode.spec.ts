bdea53de9db8abb262f397731b624b07c3636c88
brookedalton <brooke.dalton@liferay.com>
2024-10-25 10:09:35 -0700

    LPD-39639 Add test back

--- modules/test/playwright/tests/change-tracking-web/sandboxMode.spec.ts
+++ modules/test/playwright/tests/change-tracking-web/sandboxMode.spec.ts
@@ -76,7 +76,7 @@ test('LPD-34602 Add view-only mode for production when using Publications sandbo
 	await apiHelpers.headlessAdminUser.deleteUserAccount(Number(user.id));
 });
 
-test.skip('LPD-39341 Sandbox mode allows users to work on production without permissions', async ({
+test('LPD-39341 Sandbox mode allows users to work on production without permissions', async ({
 	apiHelpers,
 	changeTrackingPage,
 	ctCollection,
