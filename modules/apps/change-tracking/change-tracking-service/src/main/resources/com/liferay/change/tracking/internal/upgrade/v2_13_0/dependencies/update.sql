5b98e23891cda4559c044ca0af7f5637ab8d6a57
David Truong <david.truong@liferay.com>
2025-01-08 20:16:50 -0600

    LPD-45587 CTScore is a 1:1 mapping with CTCollection so no need for separate primary key

--- modules/apps/change-tracking/change-tracking-service/src/main/resources/com/liferay/change/tracking/internal/upgrade/v2_13_0/dependencies/update.sql
+++ modules/apps/change-tracking/change-tracking-service/src/main/resources/com/liferay/change/tracking/internal/upgrade/v2_13_0/dependencies/update.sql
@@ -1,11 +1,8 @@
 create table CTScore (
-						 mvccVersion LONG default 0 not null,
-						 ctScoreId LONG not null primary key,
-						 companyId LONG,
-						 ctCollectionId LONG,
-						 score INTEGER
+	 mvccVersion LONG default 0 not null,
+	 ctCollectionId LONG not null primary key,
+	 companyId LONG,
+	 score INTEGER
 );
-
-create unique index IX_13F5EC85 on CTScore (ctCollectionId);
 
 COMMIT_TRANSACTION;
