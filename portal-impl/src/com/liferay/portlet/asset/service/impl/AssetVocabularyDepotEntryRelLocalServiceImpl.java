38a54288b44c83e989dfb9a4161f862272efd55d
pei-jung <pei-jung.lan@liferay.com>
2025-02-18 23:36:57 -0800

    LPD-48112 Implement service

--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetVocabularyDepotEntryRelLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetVocabularyDepotEntryRelLocalServiceImpl.java
@@ -5,11 +5,100 @@
 
 package com.liferay.portlet.asset.service.impl;
 
+import com.liferay.asset.kernel.exception.InvalidAssetVocabularyDepotEntryRelException;
+import com.liferay.asset.kernel.model.AssetVocabularyDepotEntryRel;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
+import com.liferay.portal.kernel.bean.BeanReference;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portlet.asset.service.base.AssetVocabularyDepotEntryRelLocalServiceBaseImpl;
 
+import java.util.List;
+
 /**
- * @author Brian Wing Shun Chan
+ * @author Pei-Jung Lan
  */
 public class AssetVocabularyDepotEntryRelLocalServiceImpl
 	extends AssetVocabularyDepotEntryRelLocalServiceBaseImpl {
+
+	@Override
+	public AssetVocabularyDepotEntryRel addAssetVocabularyDepotEntryRel(
+			long assetVocabularyId, long depotEntryId)
+		throws PortalException {
+
+		AssetVocabularyDepotEntryRel assetVocabularyDepotEntryRel =
+			assetVocabularyDepotEntryRelPersistence.fetchByAVI_DEI(
+				assetVocabularyId, depotEntryId);
+
+		if (assetVocabularyDepotEntryRel != null) {
+			return assetVocabularyDepotEntryRel;
+		}
+
+		assetVocabularyDepotEntryRel = createAssetVocabularyDepotEntryRel(
+			counterLocalService.increment());
+
+		assetVocabularyDepotEntryRel.setAssetVocabularyId(assetVocabularyId);
+		assetVocabularyDepotEntryRel.setDepotEntryId(depotEntryId);
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext != null) {
+			assetVocabularyDepotEntryRel.setUuid(serviceContext.getUuid());
+		}
+
+		return addAssetVocabularyDepotEntryRel(assetVocabularyDepotEntryRel);
+	}
+
+	public void deleteAssetVocabularyDepotEntryRelsByAssetVocabularyId(
+		long assetVocabularyId) {
+
+		assetVocabularyDepotEntryRelPersistence.removeByAssetVocabularyId(
+			assetVocabularyId);
+	}
+
+	public void deleteAssetVocabularyDepotEntryRelsByDepotEntryId(
+		long depotEntryId) {
+
+		assetVocabularyDepotEntryRelPersistence.removeByDepotEntryId(
+			depotEntryId);
+	}
+
+	public List<AssetVocabularyDepotEntryRel>
+		getAssetVocabularyDepotEntryRelsByAssetVocabularyId(
+			long assetVocabularyId) {
+
+		return assetVocabularyDepotEntryRelPersistence.findByAssetVocabularyId(
+			assetVocabularyId);
+	}
+
+	public List<AssetVocabularyDepotEntryRel>
+		getAssetVocabularyDepotEntryRelsByDepotEntryId(long depotEntryId) {
+
+		return assetVocabularyDepotEntryRelPersistence.findByDepotEntryId(
+			depotEntryId);
+	}
+
+	public void setAssetVocabularyDepotEntryRels(
+			long assetVocabularyId, long[] depotEntryIds)
+		throws PortalException {
+
+		if (ArrayUtil.isEmpty(depotEntryIds)) {
+			throw new InvalidAssetVocabularyDepotEntryRelException(
+				"DepotEntry IDs cannot be empty");
+		}
+
+		assetVocabularyDepotEntryRelPersistence.removeByAssetVocabularyId(
+			assetVocabularyId);
+
+		for (long depotEntryId : depotEntryIds) {
+			addAssetVocabularyDepotEntryRel(assetVocabularyId, depotEntryId);
+		}
+	}
+
+	@BeanReference(type = AssetVocabularyLocalService.class)
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
+
 }
--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetVocabularyDepotEntryRelServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetVocabularyDepotEntryRelServiceImpl.java
@@ -5,11 +5,54 @@
 
 package com.liferay.portlet.asset.service.impl;
 
+import com.liferay.asset.kernel.model.AssetVocabularyDepotEntryRel;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portlet.asset.service.base.AssetVocabularyDepotEntryRelServiceBaseImpl;
+import com.liferay.portlet.asset.service.permission.AssetVocabularyPermission;
+
+import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
  */
 public class AssetVocabularyDepotEntryRelServiceImpl
 	extends AssetVocabularyDepotEntryRelServiceBaseImpl {
+
+	@Override
+	public AssetVocabularyDepotEntryRel addAssetVocabularyDepotEntryRel(
+			long assetVocabularyId, long depotEntryId)
+		throws PortalException {
+
+		AssetVocabularyPermission.check(
+			getPermissionChecker(), assetVocabularyId, ActionKeys.UPDATE);
+
+		return assetVocabularyDepotEntryRelLocalService.
+			addAssetVocabularyDepotEntryRel(assetVocabularyId, depotEntryId);
+	}
+
+	public List<AssetVocabularyDepotEntryRel>
+			getAssetVocabularyDepotEntryRelsByAssetVocabularyId(
+				long assetVocabularyId)
+		throws PortalException {
+
+		AssetVocabularyPermission.check(
+			getPermissionChecker(), assetVocabularyId, ActionKeys.VIEW);
+
+		return assetVocabularyDepotEntryRelLocalService.
+			getAssetVocabularyDepotEntryRelsByAssetVocabularyId(
+				assetVocabularyId);
+	}
+
+	public void setAssetVocabularyDepotEntryRels(
+			long assetVocabularyId, long[] depotEntryIds)
+		throws PortalException {
+
+		AssetVocabularyPermission.check(
+			getPermissionChecker(), assetVocabularyId, ActionKeys.UPDATE);
+
+		assetVocabularyDepotEntryRelLocalService.
+			setAssetVocabularyDepotEntryRels(assetVocabularyId, depotEntryIds);
+	}
+
 }
