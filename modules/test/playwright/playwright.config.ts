38ca7445af7f952aaa7da53e4431ec87c7f0f85c
Jonathan McCann <jonathan.mccann@liferay.com>
2025-02-28 14:14:14 -0800

    LPD-50281 Migrate Sites Directory poshi test to Playwright

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -116,6 +116,7 @@ import {config as siteAdminWebConfig} from './tests/site-admin-web/config';
 import {config as siteCmsSiteInitializerConfig} from './tests/site-cms-site-initializer/config';
 import {config as siteNavigationAdminWebConfig} from './tests/site-navigation-admin-web/config';
 import {config as siteNavigationBreadcrumbWebConfig} from './tests/site-navigation-breadcrumb-web/config';
+import {config as siteNavigationDirectoryWebConfig} from './tests/site-navigation-directory-web/config';
 import {config as siteNavigationLanguageWebConfig} from './tests/site-navigation-language-web/config';
 import {config as stableConfig} from './tests/stable/config';
 import {config as stagingConfig} from './tests/staging-configuration-web/config';
@@ -245,6 +246,7 @@ export default defineConfig({
 		siteCmsSiteInitializerConfig,
 		siteNavigationAdminWebConfig,
 		siteNavigationBreadcrumbWebConfig,
+		siteNavigationDirectoryWebConfig,
 		siteNavigationLanguageWebConfig,
 		stableConfig,
 		stagingConfig,
