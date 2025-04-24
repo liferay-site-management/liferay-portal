d1e121f59a7982aa9ba682c246d6a14de87aa55b
David Truong <david.truong@liferay.com>
2025-04-24 14:24:12 -0500

    LPD-52716 fix test to also ensure the publication versions is properly resolved

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/change/tracking/test/FragmentEntryVersionConstraintResolverTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/change/tracking/test/FragmentEntryVersionConstraintResolverTest.java
@@ -9,9 +9,11 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.change.tracking.conflict.ConflictInfo;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.change.tracking.service.CTProcessLocalService;
 import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.fragment.model.FragmentCollection;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryVersion;
 import com.liferay.fragment.service.FragmentCollectionLocalService;
 import com.liferay.fragment.service.FragmentEntryLocalService;
 import com.liferay.petra.lang.SafeCloseable;
@@ -20,6 +22,7 @@ import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
@@ -33,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -51,59 +55,102 @@ public class FragmentEntryVersionConstraintResolverTest {
 			new LiferayIntegrationTestRule(),
 			PermissionCheckerMethodTestRule.INSTANCE);
 
-	@Test
-	public void testResolveConflict() throws Exception {
-		Group group = GroupTestUtil.addGroup();
-
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext(group.getGroupId());
-
-		FragmentCollection fragmentCollection =
-			_fragmentCollectionLocalService.addFragmentCollection(
-				null, TestPropsValues.getUserId(), group.getGroupId(),
-				RandomTestUtil.randomString(), StringPool.BLANK,
-				serviceContext);
-
-		FragmentEntry fragmentEntry =
-			_fragmentEntryLocalService.addFragmentEntry(
-				null, TestPropsValues.getUserId(), group.getGroupId(),
-				fragmentCollection.getFragmentCollectionId(), null,
-				RandomTestUtil.randomString(), StringPool.BLANK,
-				"Fragment Entry HTML", StringPool.BLANK, false, null, null, 0,
-				false, false, FragmentConstants.TYPE_COMPONENT, null,
-				WorkflowConstants.STATUS_APPROVED, serviceContext);
-
-		CTCollection ctCollection = _ctCollectionLocalService.addCTCollection(
+	@Before
+	public void setUp() throws Exception {
+		_ctCollection = _ctCollectionLocalService.addCTCollection(
 			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
 			0, RandomTestUtil.randomString(), null);
 
+		_group = GroupTestUtil.addGroup();
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+		_fragmentCollection =
+			_fragmentCollectionLocalService.addFragmentCollection(
+				null, TestPropsValues.getUserId(), _group.getGroupId(),
+				RandomTestUtil.randomString(), StringPool.BLANK,
+				serviceContext);
+
+		_fragmentEntry = _fragmentEntryLocalService.addFragmentEntry(
+			null, TestPropsValues.getUserId(), _group.getGroupId(),
+			_fragmentCollection.getFragmentCollectionId(), null,
+			RandomTestUtil.randomString(), StringPool.BLANK,
+			"Fragment Entry HTML", StringPool.BLANK, false, null, null, 0,
+			false, false, FragmentConstants.TYPE_COMPONENT, null,
+			WorkflowConstants.STATUS_APPROVED, serviceContext);
+	}
+
+	@Test
+	public void testResolveConflict() throws Exception {
+		String html = RandomTestUtil.randomString();
+
 		try (SafeCloseable safeCloseable =
 				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
-					ctCollection.getCtCollectionId())) {
+					_ctCollection.getCtCollectionId())) {
 
-			fragmentEntry.setName(RandomTestUtil.randomString());
+			_fragmentEntry.setHtml(RandomTestUtil.randomString());
 
-			fragmentEntry = _fragmentEntryLocalService.updateFragmentEntry(
-				fragmentEntry);
+			_fragmentEntry = _fragmentEntryLocalService.updateFragmentEntry(
+				_fragmentEntry);
+
+			_fragmentEntry.setHtml(html);
+
+			_fragmentEntry = _fragmentEntryLocalService.updateFragmentEntry(
+				_fragmentEntry);
+
+			FragmentEntryVersion fragmentEntryVersion =
+				_fragmentEntryLocalService.fetchLatestVersion(_fragmentEntry);
+
+			Assert.assertEquals(3, fragmentEntryVersion.getVersion());
 		}
 
-		fragmentEntry.setName(RandomTestUtil.randomString());
+		_fragmentEntry.setHtml(RandomTestUtil.randomString());
 
-		_fragmentEntryLocalService.updateFragmentEntry(fragmentEntry);
+		_fragmentEntry = _fragmentEntryLocalService.updateFragmentEntry(
+			_fragmentEntry);
 
 		Map<Long, List<ConflictInfo>> conflictsMap =
-			_ctCollectionLocalService.checkConflicts(ctCollection);
+			_ctCollectionLocalService.checkConflicts(_ctCollection);
 
 		Assert.assertFalse(conflictsMap.isEmpty());
+
+		_ctProcessLocalService.addCTProcess(
+			TestPropsValues.getUserId(), _ctCollection.getCtCollectionId());
+
+		_fragmentEntry = _fragmentEntryLocalService.getFragmentEntry(
+			_fragmentEntry.getFragmentEntryId());
+
+		Assert.assertEquals(html, _fragmentEntry.getHtml());
+
+		FragmentEntryVersion fragmentEntryVersion =
+			_fragmentEntryLocalService.fetchLatestVersion(_fragmentEntry);
+
+		Assert.assertEquals(5, fragmentEntryVersion.getVersion());
 	}
+
+	@DeleteAfterTestRun
+	private CTCollection _ctCollection;
 
 	@Inject
 	private CTCollectionLocalService _ctCollectionLocalService;
 
 	@Inject
+	private CTProcessLocalService _ctProcessLocalService;
+
+	@DeleteAfterTestRun
+	private FragmentCollection _fragmentCollection;
+
+	@Inject
 	private FragmentCollectionLocalService _fragmentCollectionLocalService;
+
+	@DeleteAfterTestRun
+	private FragmentEntry _fragmentEntry;
 
 	@Inject
 	private FragmentEntryLocalService _fragmentEntryLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
 
 }
