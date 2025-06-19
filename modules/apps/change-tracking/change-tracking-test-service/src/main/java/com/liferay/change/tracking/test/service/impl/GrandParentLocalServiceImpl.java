6d736337f3cfda327f908045a34534d4798b318f
David Truong <david.truong@liferay.com>
2025-06-19 09:23:52 -1000

    LPD-57795 build service

---
+++ modules/apps/change-tracking/change-tracking-test-service/src/main/java/com/liferay/change/tracking/test/service/impl/GrandParentLocalServiceImpl.java
@@ -0,0 +1,89 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.change.tracking.test.service.impl;
+
+import com.liferay.change.tracking.test.model.GrandParent;
+import com.liferay.change.tracking.test.service.base.GrandParentLocalServiceBaseImpl;
+import com.liferay.change.tracking.test.service.persistence.ChildPersistence;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@Component(
+	property = "model.class.name=com.liferay.change.tracking.test.model.GrandParent",
+	service = AopService.class
+)
+public class GrandParentLocalServiceImpl
+	extends GrandParentLocalServiceBaseImpl {
+
+	@Override
+	public GrandParent addGrandParent(long companyId) {
+		return addGrandParent(companyId, 0);
+	}
+
+	@Override
+	public GrandParent addGrandParent(
+		long companyId, long parentGrandParentId) {
+
+		long grandParentId = counterLocalService.increment(
+			GrandParent.class.getName());
+
+		GrandParent grandParent = grandParentPersistence.create(grandParentId);
+
+		grandParent.setCompanyId(companyId);
+		grandParent.setName(RandomTestUtil.randomString());
+		grandParent.setParentGrandParentId(parentGrandParentId);
+
+		return grandParentPersistence.update(grandParent);
+	}
+
+	@Override
+	public GrandParent deleteGrandParent(GrandParent grandParent) {
+		_childPersistence.removeByC_G(
+			grandParent.getCompanyId(), grandParent.getGrandParentId());
+
+		return grandParentPersistence.remove(grandParent);
+	}
+
+	@Override
+	public void deleteGrandParents(long companyId) {
+		List<GrandParent> grandParents = grandParentPersistence.findByCompanyId(
+			companyId);
+
+		for (GrandParent grandParent : grandParents) {
+			deleteGrandParent(grandParent);
+		}
+	}
+
+	@Override
+	public List<GrandParent> getGrandParents(long companyId) {
+		return grandParentPersistence.findByCompanyId(companyId);
+	}
+
+	@Override
+	public GrandParent updateGrandParent(long grandParentId)
+		throws PortalException {
+
+		GrandParent grandParent = grandParentPersistence.findByPrimaryKey(
+			grandParentId);
+
+		grandParent.setName(RandomTestUtil.randomString());
+
+		return grandParentPersistence.update(grandParent);
+	}
+
+	@Reference
+	private ChildPersistence _childPersistence;
+
+}
