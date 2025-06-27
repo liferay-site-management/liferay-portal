df86a5b1b75845cea1f550a0f9ebcd8044ea40e7
Joao Victor <joao.victor@liferay.com>
2025-06-27 17:34:04 -0300

    LPD-59713 Migrate playwright tests

--- modules/test/playwright/tests/site-navigation-menu-web/main/navigationMenu.spec.ts
+++ modules/test/playwright/tests/site-navigation-menu-web/main/navigationMenu.spec.ts
@@ -6,20 +6,33 @@
 import {expect, mergeTests} from '@playwright/test';
 
 import {apiHelpersTest} from '../../../fixtures/apiHelpersTest';
-import {applicationsMenuPageTest} from '../../../fixtures/applicationsMenuPageTest';
+import {customFieldsPagesTest} from '../../../fixtures/customFieldsPagesTest';
+import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
 import {isolatedSiteTest} from '../../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../../fixtures/loginTest';
 import {pageViewModePagesTest} from '../../../fixtures/pageViewModePagesTest';
+import {pagesAdminPagesTest} from '../../../fixtures/pagesAdminPagesTest';
+import {TCustomField} from '../../../helpers/CustomFieldTypesHelper';
 import getRandomString from '../../../utils/getRandomString';
+import {pagesPagesTest} from '../../layout-admin-web/main/fixtures/pagesPagesTest';
+import {templatesPageTest} from '../../template-web/main/fixtures/templatesPageTest';
 import {navigationMenusPagesTest} from './../../site-navigation-admin-web/main/fixtures/navigationMenusPagesTest';
+import {navigationMenuWidgetPagesTest} from './fixtures/navigationMenuWidgetPagesTest';
 
 export const test = mergeTests(
 	apiHelpersTest,
-	applicationsMenuPageTest,
+	customFieldsPagesTest,
+	featureFlagsTest({
+		'LPS-178052': {enabled: true},
+	}),
 	isolatedSiteTest,
 	loginTest(),
 	navigationMenusPagesTest,
-	pageViewModePagesTest
+	pagesAdminPagesTest,
+	pagesPagesTest,
+	templatesPageTest,
+	pageViewModePagesTest,
+	navigationMenuWidgetPagesTest
 );
 
 test(
@@ -39,9 +52,7 @@ test(
 			title: getRandomString(),
 		});
 
-		await page.goto(
-			`/web${site.friendlyUrlPath}${parentLayout.friendlyURL}`
-		);
+		await widgetPagePage.goto(parentLayout, site.friendlyUrlPath);
 
 		await widgetPagePage.clickOnAction('Menu Display', 'Configuration');
 
@@ -101,7 +112,7 @@ test(
 
 		await navigationMenusPage.addURLItem(urlItemName);
 
-		await page.goto(`/web${site.friendlyUrlPath}${layout.friendlyURL}`);
+		await widgetPagePage.goto(layout, site.friendlyUrlPath);
 
 		await page.waitForTimeout(300);
 
@@ -157,7 +168,7 @@ test(
 
 		await navigationMenusPage.addURLItem(urlItemName, undefined, true);
 
-		await page.goto(`/web${site.friendlyUrlPath}${layout.friendlyURL}`);
+		await widgetPagePage.goto(layout, site.friendlyUrlPath);
 
 		await page.waitForTimeout(300);
 
@@ -196,5 +207,232 @@ test(
 		const newTabURL = newPage.url();
 
 		expect(newTabURL).toContain('https://www.liferay.com');
+	}
+);
+
+test(
+	'Show more than two sublevels when select custom menu and bar minimally styled',
+	{
+		tag: '@LPD-50258',
+	},
+	async ({
+		apiHelpers,
+		navigationMenuWidgetPage,
+		navigationMenusPage,
+		page,
+		site,
+		widgetPagePage,
+	}) => {
+
+		// Create Layouts in the same level
+
+		const layoutName1 = getRandomString();
+
+		const layoutName2 = getRandomString();
+
+		const layoutName3 = getRandomString();
+
+		const layoutName4 = getRandomString();
+
+		const layoutNames = [layoutName2, layoutName3, layoutName4];
+
+		const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
+			externalReferenceCode: getRandomString(),
+			groupId: site.id,
+			title: layoutName1,
+		});
+
+		for (let index = 0; index < 3; index++) {
+			await apiHelpers.jsonWebServicesLayout.addLayout({
+				externalReferenceCode: getRandomString(),
+				groupId: site.id,
+				title: layoutNames[index],
+			});
+		}
+
+		// Create Navigation Menu
+
+		await navigationMenusPage.goto(site.friendlyUrlPath);
+
+		const navigationMenuName = getRandomString();
+
+		await navigationMenusPage.createNavigationMenu(navigationMenuName);
+
+		// Create Page Navigation Menu Items on different levels
+
+		await navigationMenusPage.addPageItem([layoutName1]);
+
+		await navigationMenusPage.addChildPage(layoutName1, layoutName2);
+
+		await navigationMenusPage.addChildPage(layoutName2, layoutName3);
+
+		await navigationMenusPage.addChildPage(layoutName3, layoutName4);
+
+		// Select the created Navigation Menu in the Navigation Menu Widget
+
+		await widgetPagePage.goto(layout, site.friendlyUrlPath);
+
+		await navigationMenuWidgetPage.openConfigurationModal(
+			layout.nameCurrentValue
+		);
+
+		await navigationMenuWidgetPage.selectCustomNavigationMenu(
+			navigationMenuName
+		);
+
+		await navigationMenuWidgetPage.saveAndCloseConfigurationModal();
+
+		// Assert that the Navigation Menu Items are viewable when the mouse hove over the first Page Menu Item
+
+		await expect(
+			page.getByRole('menuitem', {name: layoutName1})
+		).toBeVisible();
+
+		await page.getByRole('menuitem', {name: layoutName1}).hover();
+
+		await expect(
+			page.getByRole('menuitem', {name: layoutName2})
+		).toBeVisible();
+
+		await expect(page.getByRole('link', {name: layoutName3})).toBeVisible();
+
+		await expect(page.getByRole('link', {name: layoutName4})).toBeVisible();
+	}
+);
+
+test(
+	'View custom field of Navigation Menu',
+	{
+		tag: '@LPD-50258',
+	},
+	async ({
+		addCustomFieldPage,
+		apiHelpers,
+		navigationMenuWidgetPage,
+		navigationMenusPage,
+		page,
+		site,
+		templatesPage,
+		viewAttributesPage,
+		widgetPagePage,
+	}) => {
+
+		// Create Custom Field for Navigation Menu Items
+
+		const customFieldName = 'Subtitle';
+
+		const customField: TCustomField = {
+			fieldName: customFieldName,
+			fieldType: 'inputField',
+			resource: 'Site Navigation Menu Item',
+		};
+
+		await addCustomFieldPage.addCustomField(customField);
+
+		// Create layout
+
+		const layoutName = getRandomString();
+
+		const submenuItemName = getRandomString();
+
+		const urlItemName = getRandomString();
+
+		const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
+			externalReferenceCode: getRandomString(),
+			groupId: site.id,
+			title: layoutName,
+		});
+
+		// Create Navigation Menu
+
+		await navigationMenusPage.goto(site.friendlyUrlPath);
+
+		const navigationMenuName = getRandomString();
+
+		await navigationMenusPage.createNavigationMenu(navigationMenuName);
+
+		// Create Navigation Menu Items
+
+		await navigationMenusPage.addPageItem([layoutName]);
+
+		await navigationMenusPage.addSubmenuItem(submenuItemName);
+
+		await navigationMenusPage.addURLItem(urlItemName);
+
+		// Fill the Custom Fields of the Navigation Menu Items and store its value
+
+		const value1 =
+			await navigationMenusPage.fillNavagationMenuItemCustomField(
+				layoutName,
+				customFieldName
+			);
+
+		const value2 =
+			await navigationMenusPage.fillNavagationMenuItemCustomField(
+				submenuItemName,
+				customFieldName
+			);
+
+		const value3 =
+			await navigationMenusPage.fillNavagationMenuItemCustomField(
+				urlItemName,
+				customFieldName
+			);
+
+		// Create Widget Template with a custom template script
+
+		await templatesPage.gotoWidgetTemplates(site.friendlyUrlPath);
+
+		const widgetTemplateName = getRandomString();
+
+		await templatesPage.createWidgetTemplate(
+			widgetTemplateName,
+			'Menu Display Template'
+		);
+
+		await templatesPage.editTemplate(widgetTemplateName);
+
+		await templatesPage.importInformationTemplate(
+			__dirname,
+			'custom_field_template.ftl'
+		);
+
+		await templatesPage.saveTemplate(widgetTemplateName);
+
+		// Use the created Navigation Menu in the Navigation Menu Widget and apply the created Widget Template
+
+		await widgetPagePage.goto(layout, site.friendlyUrlPath);
+
+		await navigationMenuWidgetPage.openConfigurationModal(
+			layout.nameCurrentValue
+		);
+
+		await navigationMenuWidgetPage.selectCustomNavigationMenu(
+			navigationMenuName
+		);
+
+		await page.waitForTimeout(1500);
+
+		await navigationMenuWidgetPage.selectDisplayTemplate(
+			widgetTemplateName
+		);
+
+		await navigationMenuWidgetPage.saveAndCloseConfigurationModal();
+
+		// Verify that the changes where applied
+
+		await expect(page.getByText(value1)).toBeVisible();
+
+		await expect(page.getByText(value2)).toBeVisible();
+
+		await expect(page.getByText(value3)).toBeVisible();
+
+		// Cleanup
+
+		await viewAttributesPage.goto('Site Navigation Menu Item');
+
+		await page.getByLabel('Select All Items on the Page').check();
+
+		await page.getByRole('button', {name: 'Delete'}).click();
 	}
 );
