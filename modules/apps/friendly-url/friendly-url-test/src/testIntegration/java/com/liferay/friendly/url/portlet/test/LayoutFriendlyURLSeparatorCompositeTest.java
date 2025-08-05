ae68c66d6d19bbcf71fe1a066bfe57d710fde561
Victor Galan <victor.galan@liferay.com>
2025-08-05 17:51:03 +0200

    LPD-62357 Move test to the correct owner

--- modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/portlet/test/LayoutFriendlyURLSeparatorCompositeTest.java
+++ modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/portlet/test/LayoutFriendlyURLSeparatorCompositeTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.layout.portlet.test;
+package com.liferay.friendly.url.portlet.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.friendly.url.test.util.configuration.manager.FriendlyURLSeparatorConfigurationManagerTemporarySwapper;
