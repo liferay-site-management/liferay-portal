5b9b7a7fba51e624abfe881d3448d944fe27b2ce
David Truong <david.truong@liferay.com>
2025-08-19 11:29:39 -0500

    LPD-63103 reindex asynchronously CTEntries since they are not needed immediately and causes the transaction to become extremely long

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/model/listener/CTEntryModelListener.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/model/listener/CTEntryModelListener.java
@@ -29,12 +29,16 @@ public class CTEntryModelListener extends BaseModelListener<CTEntry> {
 	public void onAfterCreate(CTEntry ctEntry) {
 		_ctClosureFactory.clearCache(ctEntry.getCtCollectionId());
 
+		_reindexCTEntry(ctEntry, "reindex");
+
 		_updateCTScore(ctEntry, true);
 	}
 
 	@Override
 	public void onAfterRemove(CTEntry ctEntry) {
 		_ctClosureFactory.clearCache(ctEntry.getCtCollectionId());
+
+		_reindexCTEntry(ctEntry, "delete");
 
 		_updateCTScore(ctEntry, false);
 	}
@@ -54,6 +58,29 @@ public class CTEntryModelListener extends BaseModelListener<CTEntry> {
 		}
 
 		_ctClosureFactory.clearCache(ctEntry.getCtCollectionId());
+
+		_reindexCTEntry(ctEntry, "reindex");
+	}
+
+	private void _reindexCTEntry(CTEntry ctEntry, String type) {
+		TransactionCommitCallbackUtil.registerCallback(
+			() -> {
+				Message message = new Message();
+
+				message.setValues(
+					HashMapBuilder.<String, Object>put(
+						"companyId", ctEntry.getCompanyId()
+					).put(
+						"ctEntryId", ctEntry.getCtEntryId()
+					).put(
+						"type", type
+					).build());
+
+				_messageBus.sendMessage(
+					CTDestinationNames.CT_ENTRY_INDEXER, message);
+
+				return null;
+			});
 	}
 
 	private void _updateCTScore(CTEntry ctEntry, boolean increment) {
