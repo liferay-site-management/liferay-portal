42bb684660e7fea9065e179ea0790d4604a9966a
brookedalton <brooke.dalton@liferay.com>
2025-12-08 12:15:25 -0800

    LPD-73438 Create a modification conflict instead of a deletion conflict

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/test/util/CTCollectionTestUtil.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/test/util/CTCollectionTestUtil.java
@@ -12,17 +12,16 @@ import com.liferay.change.tracking.service.CTCollectionServiceUtil;
 import com.liferay.change.tracking.service.CTScoreLocalServiceUtil;
 import com.liferay.document.library.kernel.model.DLFileEntryMetadata;
 import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestUtil;
-import com.liferay.journal.model.JournalArticle;
-import com.liferay.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.journal.model.JournalFolder;
+import com.liferay.journal.service.JournalFolderLocalServiceUtil;
+import com.liferay.journal.test.util.JournalFolderFixture;
 import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.petra.lang.SafeCloseable;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.test.log.LogCapture;
 import com.liferay.portal.test.log.LogEntry;
@@ -42,9 +41,11 @@ public class CTCollectionTestUtil {
 
 		Group group = GroupTestUtil.addGroup();
 
-		JournalArticle journalArticle = JournalTestUtil.addArticle(
-			group.getGroupId(), RandomTestUtil.randomString(),
-			StringPool.BLANK);
+		JournalFolderFixture journalFolderFixture = new JournalFolderFixture(
+			JournalFolderLocalServiceUtil.getService());
+
+		JournalFolder journalFolder = journalFolderFixture.addFolder(
+			group.getGroupId(), RandomTestUtil.randomString());
 
 		CTCollection ctCollection =
 			CTCollectionLocalServiceUtil.addCTCollection(
@@ -55,13 +56,11 @@ public class CTCollectionTestUtil {
 				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
 					ctCollection.getCtCollectionId())) {
 
-			journalArticle = JournalArticleLocalServiceUtil.updateArticle(
-				journalArticle.getId(), RandomTestUtil.randomString());
+			JournalTestUtil.addArticle(
+				group.getGroupId(), journalFolder.getFolderId());
 		}
 
-		JournalArticleLocalServiceUtil.deleteArticle(
-			group.getGroupId(), journalArticle.getArticleId(),
-			ServiceContextTestUtil.getServiceContext());
+		JournalFolderLocalServiceUtil.deleteFolder(journalFolder.getFolderId());
 
 		return ctCollection;
 	}
