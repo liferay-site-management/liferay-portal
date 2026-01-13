3f5df6472744fe0153b9efb0804d82f085b14d06
David Truong <david.truong@liferay.com>
2026-01-13 10:02:06 -0600

    LPD-72292 build service

---
+++ modules/apps/launch/launch-api/src/main/java/com/liferay/launch/service/LaunchEntryServiceWrapper.java
@@ -0,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.launch.service;
+
+import com.liferay.portal.kernel.service.ServiceWrapper;
+
+/**
+ * Provides a wrapper for {@link LaunchEntryService}.
+ *
+ * @author Brian Wing Shun Chan
+ * @see LaunchEntryService
+ * @generated
+ */
+public class LaunchEntryServiceWrapper
+	implements LaunchEntryService, ServiceWrapper<LaunchEntryService> {
+
+	public LaunchEntryServiceWrapper() {
+		this(null);
+	}
+
+	public LaunchEntryServiceWrapper(LaunchEntryService launchEntryService) {
+		_launchEntryService = launchEntryService;
+	}
+
+	@Override
+	public LaunchEntryService getWrappedService() {
+		return _launchEntryService;
+	}
+
+	@Override
+	public void setWrappedService(LaunchEntryService launchEntryService) {
+		_launchEntryService = launchEntryService;
+	}
+
+	private LaunchEntryService _launchEntryService;
+
+}
