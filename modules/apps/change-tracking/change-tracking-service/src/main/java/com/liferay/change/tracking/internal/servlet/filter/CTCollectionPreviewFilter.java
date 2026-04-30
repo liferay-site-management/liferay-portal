ed84b0ac3bed43a6578351412f64ae6484cc4451
David Truong <david.truong@liferay.com>
2026-04-29 22:56:26 -0500

    LPD-87850 Handle null user in CTCollectionPreviewFilter

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/servlet/filter/CTCollectionPreviewFilter.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/servlet/filter/CTCollectionPreviewFilter.java
@@ -11,6 +11,7 @@ import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
 import com.liferay.portal.kernel.change.tracking.CTCollectionPreviewThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
@@ -108,8 +109,17 @@ public class CTCollectionPreviewFilter extends BasePortalFilter {
 				PermissionThreadLocal.getPermissionChecker();
 
 			if (permissionChecker == null) {
-				permissionChecker = permissionCheckerFactory.create(
-					_portal.getUser(httpServletRequest));
+				User user = _portal.getUser(httpServletRequest);
+
+				if (user == null) {
+					processFilter(
+						CTCollectionPreviewFilter.class.getName(),
+						httpServletRequest, httpServletResponse, filterChain);
+
+					return;
+				}
+
+				permissionChecker = permissionCheckerFactory.create(user);
 			}
 
 			if (!_modelResourcePermission.contains(
