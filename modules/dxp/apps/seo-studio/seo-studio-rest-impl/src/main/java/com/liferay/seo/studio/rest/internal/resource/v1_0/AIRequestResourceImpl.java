c6c285970195e55b50f8458f9819b418b36ebf92
Noor Najjar <noor.najjar@liferay.com>
2026-05-11 10:51:38 -0700

    LPD-88575 Add aggregation field to get the sum of the counts and implement AIRequestResourceImpl

--- modules/dxp/apps/seo-studio/seo-studio-rest-impl/src/main/java/com/liferay/seo/studio/rest/internal/resource/v1_0/AIRequestResourceImpl.java
+++ modules/dxp/apps/seo-studio/seo-studio-rest-impl/src/main/java/com/liferay/seo/studio/rest/internal/resource/v1_0/AIRequestResourceImpl.java
@@ -5,9 +5,24 @@
 
 package com.liferay.seo.studio.rest.internal.resource.v1_0;
 
+import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.rest.manager.v1_0.ObjectEntryManager;
+import com.liferay.object.rest.odata.entity.v1_0.provider.EntityModelProvider;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.seo.studio.rest.dto.v1_0.AIRequest;
 import com.liferay.seo.studio.rest.resource.v1_0.AIRequestResource;
 
+import jakarta.ws.rs.core.MultivaluedMap;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,6 +33,64 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = AIRequestResource.class
 )
 public class AIRequestResourceImpl extends BaseAIRequestResourceImpl {
-}
 
-// LIFERAY-REST-BUILDER-HASH:-1307856981
+	@Override
+	public Page<AIRequest> getAIRequestsPage(Pagination pagination)
+		throws Exception {
+
+		Page<ObjectEntry> objectEntriesPage =
+			_objectEntryManager.getObjectEntries(
+				contextCompany.getCompanyId(),
+				_objectDefinitionLocalService.getObjectDefinition(
+					contextCompany.getCompanyId(), "SEOStudioAIAgent"),
+				null, null, _createDTOConverterContext(), null, pagination,
+				null, null);
+
+		return Page.of(
+			transform(objectEntriesPage.getItems(), this::_toAIRequest),
+			pagination, objectEntriesPage.getTotalCount());
+	}
+
+	@Override
+	public EntityModel getEntityModel(MultivaluedMap multivaluedMap)
+		throws Exception {
+
+		return _entityModelProvider.getEntityModel(
+			_objectDefinitionLocalService.getObjectDefinition(
+				contextCompany.getCompanyId(), "SEOStudioAIAgent"));
+	}
+
+	private DTOConverterContext _createDTOConverterContext() {
+		return new DefaultDTOConverterContext(
+			contextAcceptLanguage.isAcceptAllLanguages(), null,
+			_dtoConverterRegistry, contextHttpServletRequest, null,
+			contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
+			contextUser);
+	}
+
+	private AIRequest _toAIRequest(ObjectEntry agentObjectEntry) {
+		AIRequest aiRequest = new AIRequest();
+
+		aiRequest.setAgentName(
+			() -> GetterUtil.getString(
+				agentObjectEntry.getPropertyValue("name")));
+		aiRequest.setCount(
+			() -> GetterUtil.getInteger(
+				agentObjectEntry.getPropertyValue("totalCount")));
+
+		return aiRequest;
+	}
+
+	@Reference
+	private DTOConverterRegistry _dtoConverterRegistry;
+
+	@Reference
+	private EntityModelProvider _entityModelProvider;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference(target = "(object.entry.manager.storage.type=default)")
+	private ObjectEntryManager _objectEntryManager;
+
+}
