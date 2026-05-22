3f9a0c334f6a6cf00d99e1e6b725387903ef0f65
Noor Najjar <noor.najjar@liferay.com>
2026-05-22 16:02:53 -0700

    LPD-91406 Implement the fragment and react component for on-page screen

---
+++ modules/dxp/apps/seo-studio/seo-studio-web/src/main/java/com/liferay/seo/studio/web/internal/display/context/ViewOnPageDisplayContext.java
@@ -0,0 +1,95 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.seo.studio.web.internal.display.context;
+
+import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.URLCodec;
+import com.liferay.seo.studio.web.internal.constants.SEOStudioFDSNames;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Map;
+
+/**
+ * @author Noor Najjar
+ */
+public class ViewOnPageDisplayContext {
+
+	public ViewOnPageDisplayContext(
+		HttpServletRequest httpServletRequest, Language language,
+		ObjectEntry objectEntry, JSONArray viewsJSONArray) {
+
+		_httpServletRequest = httpServletRequest;
+		_language = language;
+		_objectEntry = objectEntry;
+		_viewsJSONArray = viewsJSONArray;
+	}
+
+	public Map<String, Object> getViewProps() {
+		String lastScanDate = null;
+
+		if (_objectEntry != null) {
+			Map<String, Object> properties = _objectEntry.getProperties();
+
+			lastScanDate = GetterUtil.getString(properties.get("requestDate"));
+		}
+
+		return HashMapBuilder.<String, Object>put(
+			"apiURL", _getAPIURL()
+		).put(
+			"emptyState", _getEmptyState()
+		).put(
+			"fdsId", SEOStudioFDSNames.INSIGHT_TYPE_SECTION
+		).put(
+			"lastScanDate", lastScanDate
+		).put(
+			"views", _viewsJSONArray
+		).build();
+	}
+
+	private String _getAPIURL() {
+		long seoStudioScanId =
+			(_objectEntry != null) ? _objectEntry.getId() : -1L;
+
+		String filterString = URLCodec.encodeURL(
+			StringBundler.concat(
+				"r_seoStudioScanToSEOStudioInsightTypes_seoStudioScanId eq '",
+				seoStudioScanId, "'"),
+			true);
+
+		return "/o/seo-studio/insight-types?filter=" + filterString;
+	}
+
+	private Map<String, Object> _getEmptyState() {
+		if (_objectEntry == null) {
+			return HashMapBuilder.<String, Object>put(
+				"description",
+				_language.get(_httpServletRequest, "run-a-scan-to-see-insights")
+			).put(
+				"title", _language.get(_httpServletRequest, "no-scans-yet")
+			).build();
+		}
+
+		return HashMapBuilder.<String, Object>put(
+			"description",
+			_language.get(
+				_httpServletRequest, "there-are-no-insights-for-the-last-scan")
+		).put(
+			"title", _language.get(_httpServletRequest, "no-insights-found")
+		).build();
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private final Language _language;
+	private final ObjectEntry _objectEntry;
+	private final JSONArray _viewsJSONArray;
+
+}
